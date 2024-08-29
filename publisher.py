1ba201a5ccbe13d4a6ee913862e1502d917ec0ad
Andre Macel Mucci <andremmucci@gmail.com>
2024-08-28 22:14:26 -0300

    update publisher

--- publisher.py
+++ publisher.py
@@ -2,6 +2,6 @@ import paho.mqtt.client as mqtt
 
 mqtt_client = mqtt.Client('meu_publisher')
 mqtt_client.connect(host="localhost", port=1883)
-mqtt_client.publish(topic="/messages", payload={"minha": "mensagem"})
+mqtt_client.publish(topic="/messages", payload="minha mensagem") #payload={"minha": "mensagem"}
 
 print('acabou')
